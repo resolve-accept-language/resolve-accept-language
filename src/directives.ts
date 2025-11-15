3f73a09ff7c513f2bc6723af62503af45f5fd654
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2025-11-15 14:06:25 -0500

    build: update dependencies

--- src/directives.ts
+++ src/directives.ts
@@ -125,6 +125,7 @@ export const getDirectives = (acceptLanguageHeader: string): IndexedDirective[] 
   }
 
   // Return the sorted directives by quality and header position.
+  // eslint-disable-next-line unicorn/no-array-sort
   return directives.sort((a, b) => {
     // Compare quality values first.
     const qualityComparison = b.quality - a.quality
